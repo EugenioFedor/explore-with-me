aacb9818ac4eef13eca4c43bdc472f4269afa16d
Damir Vafin <damirvafinnailevich@gmail.com>
2026-06-01 13:48:40 +0300

    feat: добавить DTO для параметров поиска событий (админ и публичный)

---
+++ ewm-service/src/main/java/ru/practicum/ewm/dto/PublicEventSearchParams.java
@@ -0,0 +1,26 @@
+package ru.practicum.ewm.dto;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PublicEventSearchParams {
+    private String text;
+    private List<Long> categories;
+    private Boolean paid;
+    private String rangeStart;
+    private String rangeEnd;
+    private Boolean onlyAvailable;
+    private String sort;
+    private int from;
+    private int size;
+    private HttpServletRequest request;
+}
